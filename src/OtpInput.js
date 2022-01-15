48b7e94bc58abd963058b70e6baf5cc67780999a
Mukesh Parmar <52579167+mukesh-98@users.noreply.github.com>
2022-01-15 14:58:36 +0530

    change 2

--- src/OtpInput.js
+++ src/OtpInput.js
@@ -1,4 +1,3 @@
-import React from "react";
 import {
 	Platform,
 	SafeAreaView,
@@ -7,78 +6,100 @@ import {
 	TextInput,
 	Keyboard,
 } from "react-native";
-function OtpInput({ numberOfInputs, onChangeInput, onDone, ...rest }) {
-	const [otp, setOpt] = React.useState({});
-	const inputs = new Array(numberOfInputs).fill(1).map((ele, index) => {
-		return {
-			refs: React.useRef(),
-		};
-	});
-	React.useEffect(() => {
-		inputs[0].refs.current.focus();
-	}, []);
-	const handleDoneOtp = React.useCallback(() => {
-		let doneOtp = Object.values(otp).join("");
-		if (doneOtp.length === numberOfInputs) {
-			Keyboard.dismiss();
-			onDone(doneOtp);
-		}
-	}, [otp]);
-	React.useEffect(() => {
-		() => {
-			setOpt({});
-		};
-	}, []);
-	React.useEffect(() => {
-		handleDoneOtp();
-	}, [handleDoneOtp]);
-	const styles = StyleSheet.create({
-		container: {
-			marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-			flex: 1,
-			zIndex: 1,
-			height: 50,
-			flexDirection: "row",
-			justifyContent: "center",
-			...rest.inputContainerStyles,
-		},
-		input: {
-			height: 40,
-			zIndex: 1,
-			margin: 12,
-			borderWidth: 1,
-			padding: 10,
-			width: 40,
-			borderRadius: 5,
-			backgroundColor: "white",
-			...rest.inputStyles,
-		},
-	});
-	return (
-		<SafeAreaView style={styles.container}>
-			{inputs.map((ele, index) => (
-				<TextInput
-					key={index}
-					keyboardType="numeric"
-					ref={inputs[index].refs}
-					style={styles.input}
-					onKeyPress={({ nativeEvent: { key: keyValue } }) => {
-						if (keyValue === "Backspace" && inputs[index - 1] && otp[index]) {
-							inputs[index - 1].refs.current.focus();
-						}
-					}}
-					maxLength={1}
-					onChange={({ nativeEvent: { text } }) => {
-						setOpt((prev) => ({ ...prev, [index]: text }));
-						if (inputs[index + 1] && !otp[index]) {
-							inputs[index + 1].refs.current.focus();
-						}
-						onChangeInput(text);
-					}}
-				/>
-			))}
-		</SafeAreaView>
-	);
-}
+import React, { Component } from "react";
 
-export default OtpInput;
+export default class OtpInput extends Component {
+	inputs = new Array(this.props.numberOfInputs).fill(1).map((ele, index) => {
+		return {
+			refs: React.createRef(null),
+		};
+	});
+
+	constructor(prop) {
+		super(prop);
+		this.state = {};
+	}
+
+	shouldComponentUpdate(prevState, nextState) {
+		if (prevState !== nextState) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	componentDidUpdate() {
+		this.handleDoneOtp();
+	}
+
+	handleDoneOtp = () => {
+		let doneOtp = Object.values(this.state).join("");
+		if (doneOtp.length === this.props.numberOfInputs) {
+			Keyboard.dismiss();
+			this.props.onDone(doneOtp);
+		}
+	};
+
+	componentDidMount() {
+		this.inputs[0].refs.current.focus();
+	}
+
+	componentWillUnmount() {
+		this.setState({});
+	}
+
+	render() {
+		styles = StyleSheet.create({
+			container: {
+				marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+				flex: 1,
+				zIndex: 1,
+				height: 50,
+				flexDirection: "row",
+				justifyContent: "center",
+				...this.props.inputContainerStyles,
+			},
+			input: {
+				height: 40,
+				zIndex: 1,
+				margin: 12,
+				borderWidth: 1,
+				padding: 10,
+				width: 40,
+				borderRadius: 5,
+				backgroundColor: "white",
+				...this.props.inputStyles,
+			},
+		});
+
+		return (
+			<SafeAreaView style={styles.container}>
+				{this.inputs.map((ele, index) => (
+					<TextInput
+						key={index}
+						keyboardType="numeric"
+						ref={this.inputs[index].refs}
+						style={styles.input}
+						onKeyPress={({ nativeEvent: { key: keyValue } }) => {
+							if (
+								keyValue === "Backspace" &&
+								this.inputs[index - 1] &&
+								this.state[index]
+							) {
+								this.inputs[index - 1].refs.current.focus();
+							}
+						}}
+						maxLength={1}
+						onChange={({ nativeEvent: { text } }) => {
+							this, this.setState((prev) => ({ ...prev, [index]: text }));
+							if (this.inputs[index + 1] && !this.state[index]) {
+								this.inputs[index + 1].refs.current.focus();
+							}
+							this.props.onChangeInput(text);
+						}}
+					/>
+				))}
+			</SafeAreaView>
+		);
+	}
+}
